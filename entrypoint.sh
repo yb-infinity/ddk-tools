9c110a5d36c9a9df4a6c59a5f5dea8c6cb9ef905
Drake Mazzy <6117149+DrakeMazzy@users.noreply.github.com>
2025-03-10 12:50:08 +0200

    chore(logging): Add more verbosity into entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,14 +1,34 @@
 #!/bin/sh
+set -e
+
+echo "DrupalDevKit Tools"
+
+# Log helper function
+log() {
+  if [ -n "${CI}" ]; then
+    echo "$1"
+  else
+    echo "[$(date +'%Y-%m-%d %H:%M:%S')] $1"
+  fi
+}
 
 # Check if BITBUCKET_CLONE_DIR is set and not empty
 if [ -n "${BITBUCKET_CLONE_DIR}" ]; then
-  # Change to the directory
-  cd "${BITBUCKET_CLONE_DIR}" || exit 1
-  echo "Changed directory to ${BITBUCKET_CLONE_DIR}"
+  if [ -d "${BITBUCKET_CLONE_DIR}" ]; then
+    cd "${BITBUCKET_CLONE_DIR}" || exit 1
+    log "Changed directory to ${BITBUCKET_CLONE_DIR}"
+  fi
 else
   cd /tmp || exit 1
   echo "Changed directory to /tmp"
 fi
 
-# Execute the command passed to the docker container
-exec "$@"
+# Check if arguments were provided
+if [ $# -eq 0 ]; then
+  log "No command provided, running shell"
+  exec /bin/sh
+else
+  log "Executing command: $@"
+  # Execute the command passed to the docker container
+  exec "$@"
+fi
